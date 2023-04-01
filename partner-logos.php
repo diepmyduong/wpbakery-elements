0bd0297d3e2bc6403216a7f1bba336039bccbcc3
diepmyduong <diepmyduong@outlook.com.vn>
2023-04-01 11:35:46 +0700

    Update: style css

--- partner-logos.php
+++ partner-logos.php
@@ -1,10 +1,40 @@
 <?php
 /*
 Plugin Name: Partner Logos
-Description: A custom element for displaying a list of partner company logos.
+Description: A custom element for displaying a list of partner company logos in WPBakery Page Builder.
 Version: 1.0
 Author: Your Name
 */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly.
+}
+
+// Define the custom content element for WPBakery Page Builder
+add_action( 'vc_before_init', 'partner_logos_integrateWithVC' );
+function partner_logos_integrateWithVC() {
+    vc_map( array(
+        'name' => __( 'Partner Logos', 'text-domain' ),
+        'base' => 'partner_logos',
+        'category' => __( 'Content', 'text-domain' ),
+        'params' => array(
+            array(
+                'type' => 'attach_images',
+                'heading' => __( 'Images', 'text-domain' ),
+                'param_name' => 'images',
+                'value' => '',
+                'description' => __( 'Select the images you want to display.', 'text-domain' ),
+            ),
+            array(
+                'type' => 'textfield',
+                'heading' => __( 'Image size', 'text-domain' ),
+                'param_name' => 'size',
+                'value' => '',
+                'description' => __( 'Enter the size of the images in pixels (e.g. "100").', 'text-domain' ),
+            ),
+        ),
+    ) );
+}
 
 // Define the shortcode for the custom element
 function partner_logos_shortcode($atts) {
@@ -19,7 +49,7 @@ function partner_logos_shortcode($atts) {
     $output = '<div class="partner-logos">';
 
     foreach ($images as $image) {
-        $output .= '<img src="' . $image . '" alt="" class="partner-logo" style="width:' . $size . 'px;height:auto;">';
+        $output .= '<img src="' . wp_get_attachment_url($image) . '" alt="" class="partner-logo" style="width:' . $size . 'px;height:auto;">';
     }
 
     $output .= '</div>';
